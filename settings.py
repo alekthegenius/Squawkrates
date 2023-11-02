60804ef1828fb0c817560d89a79d0f7f73c6bda4
Alek V <alekvasek@icloud.com>
2023-11-02 13:50:13 -0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,3 +1,10 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+SEARCH_KEY = os.getenv('SEARCH_KEY')
+SEARCH_ID = os.getenv('SEARCH_ID')
+
 COUNTRY="us"
 
 SEARCH_URL = "https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&num=10&gl=" + COUNTRY
